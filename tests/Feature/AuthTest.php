a8bfd3dd591fb194a2100d59a96661c42af0d709
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-07-08 09:59:28 +0400

    dev: Test Auth user and Sign up user.

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_login_success(): void
+    {
+        $user = User::factory(['password' => 'password'])->create();
+
+        $this->post('/login', ['email' => $user->email, 'password' => 'password'])
+            ->assertRedirect('/');
+    }
+
+    public function test_login_bad_credential(): void
+    {
+        $this->post('/login', ['email' => 'test@test.com', 'password' => 'pass'])
+            ->assertSessionHasErrors(['email' => 'These credentials do not match our records.']);
+    }
+
+    public function test_login_validation_error(): void
+    {
+        $this->post('/login', [])
+            ->assertSessionHasErrors([
+                'email' => 'The email field is required.',
+                'password' => 'The password field is required.'
+            ], ':message');
+    }
+
+    public function test_logout(): void
+    {
+        $user = User::factory()->create();
+        Auth::setUser($user);
+
+        $this->assertAuthenticatedAs($user);
+
+        $this->post('/logout')
+            ->assertRedirect('/');
+
+        $this->assertNull(Auth::user());
+    }
+}
---
+++ tests/Feature/SignUpTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SignUpTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @dataProvider regUser
+     */
+    public function test_sign_up_validation_error_required_fields(array $data, array $error, array $noError = []): void
+    {
+        $response = $this->post('/register', $data)
+            ->assertSessionHasErrors($error)
+            ->assertRedirect('/');
+
+        if ($noError) {
+            $response->assertSessionDoesntHaveErrors($noError);
+        }
+    }
+
+    public function regUser(): array
+    {
+        return [
+            [
+                // Required field
+                ['email', 'name', 'password'],
+                [
+                    'email' => 'The email field is required.',
+                    'name' => 'The name field is required.',
+                    'password' => 'The password field is required.',
+                ],
+            ],
+            [
+                // Valid email, and min password length
+                ['email' => 'a', 'name' => 'a', 'password' => 'a'],
+                [
+                    'email' => 'The email field must be a valid email address.',
+                    'password' => 'The password must be at least 8 characters.',
+                ],
+                ['name']
+            ],
+            [
+                // password match
+                ['email' => 'a@a.com', 'name' => 'a', 'password' => '12345678'],
+                [
+                    'password' => 'The password field confirmation does not match.',
+                ],
+                ['email', 'name']
+            ],
+            [
+                // password match 2
+                ['email' => 'a@a.com', 'name' => 'a', 'password' => '12345678', 'password_confirmation' => '87654321'],
+                [
+                    'password' => 'The password field confirmation does not match.',
+                ],
+                ['email', 'name']
+            ],
+        ];
+    }
+
+    public function test_sign_up_success(): void
+    {
+        $response = $this->post('/register', [
+            'email' => 'oleg@petrov.com',
+            'name' => 'Oleg Petrov',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ])
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/');
+
+        $this->followRedirects($response)
+            ->assertStatus(200)
+            ->assertSeeText('Sign out as Oleg Petrov');
+
+        $this->assertAuthenticated();
+
+        $this->assertDatabaseHas(User::class, [
+            'email' => 'oleg@petrov.com', 'name' => 'Oleg Petrov',
+        ]);
+    }
+}
